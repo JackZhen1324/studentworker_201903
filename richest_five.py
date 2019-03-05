6a948426aaedb144662c8eafb413a59eb7ed6caf
Nathan Plamondon <nplamond@asu.edu>
2019-03-05 13:19:20 -0700

    Initial commit

---
+++ richest_five.py
@@ -0,0 +1,15 @@
+!#/usr/bin/env python
+# This script should read the accounts.json file and print out the five
+# wealthiest accounts at the bank, in order from highest to lowest balance.
+# Output should take the following form:
+# <last name>, <first name>: $<balance>
+with open('acountsjson') as jsonfile:
+    accounts = json.load(jsonfile)
+    accounts_sorted = sorted(accounts, key=lambda account = account['balance'])
+    accounts_out = json.dumps(accounts_sorted, indent=4, seperators=(',', ': '))
+    with open('accounts.tmp', 'w') as outfile;
+        outfile.write(accounst_out)
+    for account in accounts_sorted[5:]:
+        print('{}, {}: ${}'.format(account['name']['last'],
+                                   account['name']['first'],
+                                   account['balance']))
