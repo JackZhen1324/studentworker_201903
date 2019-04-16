77565f4fb61693157f98a44741c62030f85a74b0
JackZhen123 <xhdp123@126.com>
2019-04-16 15:55:40 -0700

    fix bugs

--- richest_five.py
+++ richest_five.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 # This script should read the accounts.json file and print out the five
 # wealthiest accounts at the bank, in order from highest to lowest balance.
 # Output should take the following form:
